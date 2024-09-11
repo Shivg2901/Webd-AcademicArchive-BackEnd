2cb62f693c35b90da9daebd3d0a6f5045c51e770
Shivg2901 <guptashivansh374@gmail.com>
2024-09-11 17:07:36 +0530

    deployment config

--- dist/config/db.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Role = exports.prismaClient = void 0;
-const client_1 = require("@prisma/client");
-Object.defineProperty(exports, "Role", { enumerable: true, get: function () { return client_1.Role; } });
-const prismaClient = new client_1.PrismaClient();
-exports.prismaClient = prismaClient;
